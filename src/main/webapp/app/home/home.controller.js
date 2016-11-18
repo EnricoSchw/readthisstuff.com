3a9691342c6ea650b45133be724ab2193ea24701
Enrico Schwendig <enrico.schwendig@devbliss.com>
2016-11-19 00:11:20 +0100

    Read list of documents from backend service.

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,26 +5,30 @@
         .module('rtsApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'DocumentRTS'];
 
-    function HomeController($scope, Principal, LoginService, $state) {
+    function HomeController($scope, Principal, LoginService, $state, DocumentRTS) {
         var vm = this;
 
-        vm.stuffSet = [
-            [{id: 1}, {id: 2}, {id: 3}],
-            [{id: 4}, {id: 5}, {id: 6}],
-            [{id: 7}, {id: 8}, {id: 9}]
-        ];
+        // vm.stuffSet = [
+        //     [{id: 1}, {id: 2}, {id: 3}],
+        //     [{id: 4}, {id: 5}, {id: 6}],
+        //     [{id: 7}, {id: 8}, {id: 9}]
+        // ];
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+
         $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
+
+        loadAll();
+
 
         function getAccount() {
             Principal.identity().then(function (account) {
@@ -35,6 +39,46 @@
 
         function register() {
             $state.go('register');
+        }
+
+
+        function loadAll() {
+            DocumentRTS.query(function (result) {
+                var subStuffSet = [],
+                    stuffSet = [],
+                    i = 0;
+
+                angular.forEach(result, function (value, key) {
+
+                    value = fillUp(value);
+
+                    subStuffSet.push(value);
+
+
+                    if (i % 3 == 2) {
+                        this.push(subStuffSet);
+                        subStuffSet = []
+                    }
+                    i++;
+                }, stuffSet);
+
+                if (i % 3 != 0) {
+                    stuffSet.push(stuffSet.push(subStuffSet));
+                }
+                console.log(stuffSet);
+                vm.stuffSet = stuffSet;
+            });
+        }
+
+        function fillUp(doc) {
+            if (isBlank(doc.title))
+                doc.title = "Unknown";
+
+            return doc;
+        }
+
+        function isBlank(string) {
+            return (!string || !string.length);
         }
     }
 })();
